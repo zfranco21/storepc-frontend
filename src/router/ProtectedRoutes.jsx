080d83f0e3a93469c6187937d37d757501d3352b
alfarofernando <alfarofernando@live.com>
2024-12-13 17:33:03 -0300

    Añadidas dependencias e implementacion del Router para direcciones de la aplicacion

---
+++ src/router/ProtectedRoutes.jsx
@@ -0,0 +1,23 @@
+// ProtectedRoutes.jsx
+import React from "react";
+import { Navigate, Outlet } from "react-router-dom";
+import { useAuth } from "../context/UserAuthContext";
+
+export default function ProtectedRoutes({ adminOnly = false }) {
+  const { user } = useAuth();
+
+  // Si el usuario no está definido, carga desde localStorage como respaldo
+  const savedUser = localStorage.getItem("user");
+  const currentUser = user || (savedUser && JSON.parse(savedUser));
+
+  if (!currentUser) {
+    return <Navigate to="/" />; // redireccion por si no hay usuario logeado
+  }
+
+  if (adminOnly && !currentUser.isAdmin) {
+    return <Navigate to="/" />; // redireccion por si el usuario logeado es administrador
+  }
+  }
+
+  return <Outlet />;
+}
