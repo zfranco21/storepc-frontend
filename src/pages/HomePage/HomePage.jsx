ec58c4f71ecfe455bb8126adb1a9bd69c283d7c1
Franco Herrera <francoherrera1759@gmail.com>
2024-12-16 12:42:31 -0300

    <add> Hook para traer productos del backend y renderizarlos en el frontend

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -7,9 +7,20 @@ import Carrito from '../../components/carrito';
 import Anuncio from '../../components/Anuncio'; 
 import Blackweek from '../../components/Blackweek';
 import Sponsor from '../../components/sponsor'; 
-import Productos from '../ProductosPage/ProductosPage';
+import ProductGrid from '../ProductosPage/components/ProductGrid'; 
+import { useFetchProducts } from '../../hooks/useFetchProducts';
 
 const HomePage = () => {
+  const { products, loading, error } = useFetchProducts(); // para Obtener los productos
+
+  if (loading) {
+    return <p>Cargando productos...</p>;
+  }
+
+  if (error) {
+    return <p>Error al cargar productos: {error}</p>;
+  }
+
   return (
     <>
       <Header />
@@ -17,7 +28,8 @@ const HomePage = () => {
       <main>
         <Anuncio />
         <Blackweek />
-        <Productos />
+        {/* Pasar los productos a ProductGrid */}
+        <ProductGrid products={products} />
       </main>
       <footer>
         <Sponsor />
